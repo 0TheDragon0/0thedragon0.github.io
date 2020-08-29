ea4f5b4698f09db84bd7d5f20089122694c1e016
0TheDragon0 <arianajavadi3@gmail.com>
2020-08-29 12:00:10 +0200

    Created boilerplate for sign up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
