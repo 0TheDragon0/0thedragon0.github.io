d8a9ceb5ab2bbe96159893b7356baaea67a241f7
0TheDragon0 <arianajavadi3@gmail.com>
2020-09-12 11:49:32 +0200

    Added input validation

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -21,20 +21,33 @@ export class SignUpComponent implements OnInit {
       '',
       [
         Validators.required,
-        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
-       ]
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$') //this is for the letters (both uppercase and lowercase) and numbers validation
+      ]
     ]
   });
 
-  getErrorMessage() {
-    if (this.signUpForm.controls.email.hasError('required')) {
+  getErrorMessage(controlName: string): string {
+    if (this.signUpForm.controls[controlName].hasError('required')) {
       return 'You must enter a value';
     }
+    if (controlName == 'email') {
+      return this.signUpForm.controls.email.hasError('email') ? 'Not a valid email' : '';
+    }
 
-    return this.signUpForm.controls.email.hasError('email') ? 'Not a valid email' : '';
+    if (controlName == 'username') {
+      return this.signUpForm.controls.username.hasError('minlength') ? 'Minimum length 3 characters' : '';
+    }
+
+    if (controlName == 'password') {
+      return this.signUpForm.controls.password.hasError('pattern') ? 'Minimum 8 characters with uppercase and number' : '';
+    }
   }
 
   signUp() {
     console.log('user clicked sign up button');
+  }
+
+  displayCurrentValue() {
+    return JSON.stringify(this.signUpForm.value);
   }
 }
