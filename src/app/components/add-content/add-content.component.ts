f7e1ed256d4879a7cfd94608a2adeff089e34c01
0TheDragon0 <arianajavadi3@gmail.com>
2021-05-15 11:33:05 +0200

    Customized stepper to use one form

--- src/app/components/add-content/add-content.component.ts
+++ src/app/components/add-content/add-content.component.ts
@@ -8,17 +8,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddContentComponent implements OnInit {
  
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  contentForm: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+    this.contentForm = this._formBuilder.group({
+      contentBody: ['', Validators.required], 
+      tagsCategories: ['', Validators.required]
     });
   }
 
